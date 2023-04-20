f269188ef400f202dbc8a8be82c8ad84898c7675
Dalyn Baldwin <101284161+dbdubz@users.noreply.github.com>
2023-04-20 17:22:37 -0600

    Hope this works

--- Models/Agents.cs
+++ Models/Agents.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 // Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
 // If you have enabled NRTs for your project, then un-comment the following line:
@@ -9,6 +10,8 @@ namespace Final_dlbaldwi.Models
 {
     public partial class Agents
     {
+
+        [Key]
         public long AgentId { get; set; }
         public string AgtFirstName { get; set; }
         public string AgtLastName { get; set; }
